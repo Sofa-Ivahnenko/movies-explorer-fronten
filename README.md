f4fbb6c3cf9bfc8282d8361ea2a769a862468eae
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-07-18 21:28:58 +0300

    verstka

---
+++ README.md
@@ -0,0 +1,6 @@
+# movies-explorer-frontend
+- Backend: https://api.diplom.movies.nomoredomains.wor
+- [Макет Дипломной работы в Figma](https://www.figma.com/file/ipcOMafEhdScym4b819MTW/Diploma-(Copy)?type=design&node-id=932%3A4182&mode=design&t=gzebBQsjZugEylgV-1)
+- Frontend: https://diplom.movies.nomoredomains.work
+
+IP: 130.193.38.81
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,28 @@
+import './SearchForm.css';
+
+const SearchForm = () => {
+	return (
+		<form className="search">
+			<div className="search__container">
+				<input className="search__input" placeholder="Фильм" type="text" required />
+				<button type="submit" className="search__button"></button>
+				<div className="search__toggle">
+					<label className="search__tumbler">
+						<input type="checkbox" className="search__checkbox" />
+						<span className="search__slider" />
+					</label>
+					<p className="search__films">Короткометражки</p>
+				</div>
+			</div>
+			<div className="search__toggle search__toggle-update">
+				<label className="search__tumbler">
+					<input type="checkbox" className="search__checkbox" />
+					<span className="search__slider" />
+				</label>
+				<p className="search__films">Короткометражки</p>
+			</div>
+		</form>
+	);
+};
+
+export default SearchForm;
